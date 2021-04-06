037d1e4bb17e794c38f70c675e88def0b66dd910
Niels <74902241+NielsHak@users.noreply.github.com>
2021-04-06 11:54:20 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,22 @@
 const textContainer = document.querySelector('.text-container');
-const button = document.querySelector('button');
+const button = document.querySelector('.button');
 const body = document.querySelector('body');
 const links = document.querySelector('.nav-link');
 const hamburgerLines = document.querySelectorAll('.hamburger-line');
 
-button.addEventListener('click', function() {
-    textContainer.classList.toggle('dark-mode');
-    body.classList.toggle('dark-mode');
-    hamburgerLines.forEach.classList.toggle('.hamburger-dark');
+button.addEventListener('click', () => {
+    textContainer.classList.toggle('dark-mode')
+    body.classList.toggle('dark-mode')
     if(button.innerHTML === 'enter light-mode') {
-        button.innerHTML = 'enter dark-mode';
-        button.classList.remove('btn-light');
-        body.classList.remove('dark-background');
+        button.innerHTML = 'enter dark-mode'
+        button.classList.remove('btn-light')
+        body.classList.remove('dark-background')
         } else {
-            button.innerHTML = 'enter light-mode';
-            button.classList.add('btn-light');
-            body.classList.add('dark-background');
+            button.innerHTML = 'enter light-mode'
+            button.classList.add('btn-light')
+            body.classList.add('dark-background')
         }
-    
-})
-
-
-
-// const tabItems = document.querySelectorAll('.tab-item');
-
-// function selectItem(e){
-//     removeBorder();
-//     removeShow();
-//     this.classList.add('tab-border');
-// }
-
-// function removeBorder(){
-//     tabItems.forEach(item => item.classList.remove('tab-border'));
-// }
-
-// tabItems.forEach(item => item.addEventListener('click', selectItem));
+});
 
 
 const tabs = document.querySelectorAll('[data-tab-target]');
@@ -52,7 +34,7 @@ tabs.forEach(tab => {
         target.classList.add('show')
         tab.classList.add('active')
     })
-})
+});
 
 
 
